368232cdc9842328d655922e826997ffb10f442b
Joakim Cars <joakim.cars@gmail.com>
2017-11-10 11:51:20 +0100

    classes suggestion

---
+++ Bibblan GA/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibblan_GA
+{
+    abstract class Item
+    {
+        private string title;
+        private string author;
+        private string genre;
+        private int isbn;
+        private bool availability;
+        private string type;
+
+        static int isbnCounter = 1000;
+
+        public Item(string inTitle, string inAuthor, string ingenre)
+        {
+            isbnCounter++;
+
+            title = inTitle;
+            author = inAuthor;
+            genre = ingenre;
+            isbn = isbnCounter;
+            availability = true;
+        }
+        public virtual void Rent(string customerName, int customerAge)
+        {
+
+        }
+            
+    }
+}
