0d051b096d4f1a676a2f6b5eb6ab751e25da6a60
Joakim Cars <joakim.cars@gmail.com>
2017-11-10 13:27:43 +0100

    classes ide

---
+++ Bibblan GA/Library.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibblan_GA
+{
+    class Library
+    {
+        List<Item> books = new List<Item>
+        {
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            new Book("the Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+        };
+    }
+}
---
+++ Bibblan GA/SoundBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibblan_GA
+{
+    class SoundBook : Item
+    {
+        private string type;
+
+        public SoundBook(string inName, string inAuthor, string ingenre) : base(inName, inAuthor, ingenre)
+        {
+            type = "soundbook";
+
+        }
+
+        public override void Rent(string customerName, int customerAge)
+        {
+            base.Rent(customerName, customerAge);
+        }
+    }
+}
